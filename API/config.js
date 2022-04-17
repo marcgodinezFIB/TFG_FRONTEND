69ee55009ad715b85153135ea019ccf82702b33e
marcgodinez <marcgodinez98@gmail.com>
2022-04-17 19:54:20 +0200

    remove api and front folder

--- API/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb+srv://marc:marc@cluster0.8nfle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    SECRET_TOKEN: 'miclavedetokens'
-}
