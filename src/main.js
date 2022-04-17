7a05fe26dba753424bd78cf076416cca22c54795
marcgodinez <marcgodinez98@gmail.com>
2022-04-17 20:53:06 +0200

    url change

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ Vue.use(BootstrapVueIcons)
 Vue.use(IconsPlugin)
 Vue.use(VueAxios, axios)
 
-axios.defaults.baseURL = 'http://localhost:3000/api/';
+axios.defaults.baseURL = 'http://footprintscanv00.herokuapp.com/api/';
 Vue.config.productionTip = true
 //axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.token}`;
 
